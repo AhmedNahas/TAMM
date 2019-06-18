1de63924be830920ff72d873c0e483e4cd4cdce6
middle east <Middle-east1>
2019-06-18 14:48:55 +0200

    booking and others

---
+++ app/src/main/java/net/middledleeast/tamm/activities/RoomBooked.java
@@ -0,0 +1,15 @@
+package net.middledleeast.tamm.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.middledleeast.tamm.R;
+
+public class RoomBooked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_booked);
+    }
+}
