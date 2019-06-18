1138fbf0e904ccdb19fea16033104775f71e87c4
middle <middleeastapp67@gmail.com>
2019-06-18 15:02:58 +0200

    flightsorhotels xml

--- app/src/main/java/net/middledleeast/tamm/activities/FlightsOrHotels.java
+++ app/src/main/java/net/middledleeast/tamm/activities/FlightsOrHotels.java
@@ -2,12 +2,11 @@ package net.middledleeast.tamm.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
 import net.middledleeast.tamm.R;
 
 public class FlightsOrHotels extends AppCompatActivity {
-    Button bestFlights
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
