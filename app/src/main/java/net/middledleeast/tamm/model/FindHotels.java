4bf7701c1a988ebb3c16e2b265cd354a4f0608ae
Middle East <info@middledleast.net>
2019-06-19 12:30:05 +0200

    find hotels editing

--- app/src/main/java/net/middledleeast/tamm/model/FindHotels.java
+++ app/src/main/java/net/middledleeast/tamm/model/FindHotels.java
@@ -11,5 +11,7 @@ public class FindHotels extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_hotels);
+
+
     }
 }
