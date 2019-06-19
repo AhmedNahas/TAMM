8c01f3693385b6f6f8421267460e400e6325b165
middle east <Middle-east1>
2019-06-19 16:20:50 +0200

    free and member

---
+++ app/src/main/java/net/middledleeast/tamm/activities/PAYMENT.java
@@ -0,0 +1,15 @@
+package net.middledleeast.tamm.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.middledleeast.tamm.R;
+
+public class PAYMENT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
