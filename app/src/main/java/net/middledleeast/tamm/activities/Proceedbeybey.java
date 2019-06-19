a6b90f88b205cf41e423c734742131124f13641d
middle east <Middle-east1>
2019-06-19 17:15:00 +0200

    free and member

---
+++ app/src/main/java/net/middledleeast/tamm/activities/Proceedbeybey.java
@@ -0,0 +1,15 @@
+package net.middledleeast.tamm.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.middledleeast.tamm.R;
+
+public class Proceedbeybey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.proceedbey_bey);
+    }
+}
