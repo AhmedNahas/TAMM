dd058ea4aab0ee3644b5672077a11a22bd12cae7
middle east <Middle-east1>
2019-06-25 17:02:44 +0200

    new activity

---
+++ app/src/main/java/net/middledleeast/tamm/activities/RenewAccount.java
@@ -0,0 +1,106 @@
+package net.middledleeast.tamm.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
+
+import net.middledleeast.tamm.R;
+import net.middledleeast.tamm.model.ReserveRoom;
+
+public class RenewAccount extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    ImageView img1 , img2;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.renew_account);
+        img2 = findViewById(R.id.imageView10);
+        img1 = findViewById(R.id.imageView9);
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(RenewAccount.this, ReserveRoom.class);
+                startActivity(intent);
+            }
+        });
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RenewAccount.this,FlightTamm.class));
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        return true;
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_offer) {
+
+
+        } else if (id == R.id.nav_conversation) {
+
+        } else if (id == R.id.nav_favorites) {
+
+        } else if (id == R.id.nav_aboutus) {
+
+        } else if (id == R.id.nav_contuctus) {
+
+        }else if (id == R.id.nav_setting) {
+
+        }else if (id == R.id.nav_terms) {
+
+        }else if (id == R.id.nav_logout) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
