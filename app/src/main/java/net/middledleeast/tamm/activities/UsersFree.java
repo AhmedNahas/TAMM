c14b69656e3454e3ced9c364e8511635cd026f70
shahawi <shahawi273@gmail.com>
2019-06-26 15:06:58 +0200

    hotel apis library

--- app/src/main/java/net/middledleeast/tamm/activities/UsersFree.java
+++ app/src/main/java/net/middledleeast/tamm/activities/UsersFree.java
@@ -1,10 +1,11 @@
 package net.middledleeast.tamm.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import net.middledleeast.tamm.R;
 import net.middledleeast.tamm.adapters.FreeAdapter;
