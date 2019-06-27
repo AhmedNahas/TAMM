0a5ef64ecea48df45931e89386ec47c00e6a4960
shahawi <shahawi273@gmail.com>
2019-06-27 11:52:05 +0200

    testHotelapi

--- app/src/main/java/net/middledleeast/tamm/activities/TestHotelApi.java
+++ app/src/main/java/net/middledleeast/tamm/activities/TestHotelApi.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.AmendmentRequest;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.AmendmentResponse;
-import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.ArrayOfHotels;
+import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.AuthenticationData;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.AvailabilityAndPricingRequest;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.AvailabilityAndPricingResponse;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.CityWiseNotificationRequest;
@@ -40,97 +40,76 @@ import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.HotelSearchReq
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.HotelSearchResponse;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.HotelSearchWithRoomsRequest;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.HotelSearchWithRoomsResponse;
+import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.Hotels;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.IHotelService;
+import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.ObjectFactory;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.TagInfoRequest;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.TagInfoResponse;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.TopDestinationsRequest;
 import net.middledleeast.tamm.hotellibs.tektravel.hotelbookingapi.TopDestinationsResponse;
 
-import javax.xml.namespace.QName;
-import javax.xml.soap.MessageFactory;
-import javax.xml.soap.MimeHeaders;
-import javax.xml.soap.SOAPBody;
-import javax.xml.soap.SOAPConnection;
-import javax.xml.soap.SOAPConnectionFactory;
-import javax.xml.soap.SOAPConstants;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPEnvelope;
-import javax.xml.soap.SOAPHeader;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.soap.SOAPPart;
-import com.sun.xml.messaging.saaj.*;
-import com.sun.xml.messaging.saaj.soap.MessageFactoryImpl;
-import com.sun.xml.messaging.saaj.soap.SOAPPartImpl;
-import com.sun.xml.messaging.saaj.soap.ver1_2.Message1_2Impl;
-import com.sun.xml.messaging.saaj.soap.ver1_2.SOAPMessageFactory1_2Impl;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 
 public class TestHotelApi extends AppCompatActivity implements IHotelService {
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try
-        {
-        // Create SOAP Connection
-        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+    ObjectFactory objectFactory;
 
-        // Send SOAP Message to SOAP Server
-        String endpoint = "http://api.tbotechnology.in/HotelAPI_V7/HotelService.svc";
-        SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), endpoint);
-        soapResponse.writeTo(System.out);
-
-        soapConnection.close();
-    } catch (Exception e) {
-        System.err.println("Error occurred while sending SOAP Request to Server");
-        e.printStackTrace();
+    public static XMLGregorianCalendar toXMLGregorianCalendar(Calendar c)
+            throws DatatypeConfigurationException {
+        GregorianCalendar gc = new GregorianCalendar();
+        gc.setTimeInMillis(c.getTimeInMillis());
+        XMLGregorianCalendar xc = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+        return xc;
     }
-//        HotelSearchRequest hotelSearchRequest = new HotelSearchRequest();
+
+
+//    private static SOAPMessage createSOAPRequest() throws Exception {
+//        MessageFactoryImpl messageFactory = (MessageFactoryImpl) MessageFactoryImpl.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
+//        SOAPMessage soapMessage = messageFactory.createMessage();
+//        SOAPPartImpl soapPart = (SOAPPartImpl) soapMessage.getSOAPPart();
 //
-//         String test= String.valueOf(hotelSearchRequest.getResultCount());
-//        System.out.println("Heio: "+test);
-    }
-    private static SOAPMessage createSOAPRequest() throws Exception {
-        MessageFactoryImpl messageFactory = (MessageFactoryImpl) MessageFactoryImpl.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
-        SOAPMessage soapMessage = messageFactory.createMessage();
-        SOAPPartImpl soapPart = (SOAPPartImpl) soapMessage.getSOAPPart();
-
-        String serverURI = "http://TekTravel/HotelBookingApi";
-
-        // SOAP Envelope
-        SOAPEnvelope envelope = soapPart.getEnvelope();
-        envelope.addNamespaceDeclaration("hot", serverURI);
-
-        // SOAP Header
-        SOAPHeader header = envelope.getHeader();
-        header.addNamespaceDeclaration("wsa", "http://www.w3.org/2005/08/addressing");
-        SOAPElement soapHeaderElem = header.addChildElement("Credentials", "hot");
-        soapHeaderElem.addAttribute(new QName("UserName"), "kuldeep");
-        soapHeaderElem.addAttribute(new QName("Password"), "54321");
-        SOAPElement soapHeaderElem1 = header.addChildElement("Action", "wsa");
-        soapHeaderElem1.addTextNode("http://TekTravel/HotelBookingApi/DestinationCityList");
-        SOAPElement soapHeaderElem2 = header.addChildElement("To", "wsa");
-        soapHeaderElem2.addTextNode("http://api.tbotechnology.in/HotelAPI_V7/HotelService.svc");
-
-        // SOAP Body
-        SOAPBody soapBody = envelope.getBody();
-
-        SOAPElement soapBodyElem = soapBody.addChildElement("DestinationCityListRequest", "hot");
-        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("CountryCode", "hot");
-        soapBodyElem1.addTextNode("IN");
-
-        MimeHeaders headers = soapMessage.getMimeHeaders();
-        headers.addHeader("Content-Type", "application/soap+xml;charset=UTF-8");
-
-        soapMessage.saveChanges();
-
-        /* Print the request message */
-        System.out.print("Request SOAP Message = ");
-        soapMessage.writeTo(System.out);
-        System.out.println();
-
-        return soapMessage;
-    }
+//        String serverURI = "http://TekTravel/HotelBookingApi";
+//
+//        // SOAP Envelope
+//        SOAPEnvelope envelope = soapPart.getEnvelope();
+//        envelope.addNamespaceDeclaration("hot", serverURI);
+//
+//        // SOAP Header
+//        SOAPHeader header = envelope.getHeader();
+//        header.addNamespaceDeclaration("wsa", "http://www.w3.org/2005/08/addressing");
+//        SOAPElement soapHeaderElem = header.addChildElement("Credentials", "hot");
+//        soapHeaderElem.addAttribute(new QName("UserName"), "kuldeep");
+//        soapHeaderElem.addAttribute(new QName("Password"), "54321");
+//        SOAPElement soapHeaderElem1 = header.addChildElement("Action", "wsa");
+//        soapHeaderElem1.addTextNode("http://TekTravel/HotelBookingApi/DestinationCityList");
+//        SOAPElement soapHeaderElem2 = header.addChildElement("To", "wsa");
+//        soapHeaderElem2.addTextNode("http://api.tbotechnology.in/HotelAPI_V7/HotelService.svc");
+//
+//        // SOAP Body
+//        SOAPBody soapBody = envelope.getBody();
+//
+//        SOAPElement soapBodyElem = soapBody.addChildElement("DestinationCityListRequest", "hot");
+//        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("CountryCode", "hot");
+//        soapBodyElem1.addTextNode("IN");
+//
+//        MimeHeaders headers = soapMessage.getMimeHeaders();
+//        headers.addHeader("Content-Type", "application/soap+xml;charset=UTF-8");
+//
+//        soapMessage.saveChanges();
+//
+//        /* Print the request message */
+//        System.out.print("Request SOAP Message = ");
+//        soapMessage.writeTo(System.out);
+//        System.out.println();
+//
+//        return soapMessage;
+//    }
 
     @Override
     public HotelBookingDetailResponse hotelBookingDetail(HotelBookingDetailRequest parameters) {
@@ -228,10 +207,55 @@ public class TestHotelApi extends AppCompatActivity implements IHotelService {
     }
 
     @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        try
+//        {
+//        // Create SOAP Connection
+//        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+//        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+//
+//        // Send SOAP Message to SOAP Server
+//        String endpoint = "http://api.tbotechnology.in/HotelAPI_V7/HotelService.svc";
+//        SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), endpoint);
+//        soapResponse.writeTo(System.out);
+//
+//        soapConnection.close();
+//    } catch (Exception e) {
+//        System.err.println("Error occurred while sending SOAP Request to Server");
+//        e.printStackTrace();
+//    }
+        objectFactory = new ObjectFactory();
+        AuthenticationData authenticationData = objectFactory.createAuthenticationData();
+        authenticationData.setSiteName("https://api.tbotechnology.in/hotelapi_v7/hotelservice.svc");
+        authenticationData.setUserName("Tammtest");
+        authenticationData.setPassword("Tam@18418756");
+        objectFactory.createCredentials(authenticationData);
+        objectFactory.createAPIHotelDetails();
+        objectFactory.createAccountInfo();
+        HotelSearchRequest hotelSearchRequest = objectFactory.createHotelSearchRequest();
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(Calendar.DAY_OF_MONTH, 28);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.DAY_OF_MONTH, 30);
+        try {
+            hotelSearchRequest.setCheckInDate(toXMLGregorianCalendar(cal1));
+            hotelSearchRequest.setCheckOutDate(toXMLGregorianCalendar(cal2));
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+        HotelSearchResponse hotelSearchResponse = objectFactory.createHotelSearchResponse();
+
+        Hotels hotels = objectFactory.createHotels();
+//       String test = String.valueOf(2);
+//        HotelService hotelService = new HotelService();
+        System.out.println("Heio: " + hotelSearchResponse.getStatus());
+    }
+
+    @Override
     public HotelSearchResponse hotelSearch(HotelSearchRequest parameters) {
 
 
-
-        return null;
+        return new HotelSearchResponse();
     }
 }
