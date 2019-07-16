cc49fe5cf370b4d54999b44e495804e940938d80
Middle East <info@middledleast.net>
2019-07-16 17:10:20 +0200

    nothing
    Signed-off-by: Middle East <info@middledleast.net>

--- app/src/main/java/net/middledleeast/tamm/adapters/FromToRvAdapter.java
+++ app/src/main/java/net/middledleeast/tamm/adapters/FromToRvAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,12 +19,16 @@ public class FromToRvAdapter extends RecyclerView.Adapter<FromToRvAdapter.FromTo
     @NonNull
     @Override
     public FromToRvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_dialog,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.from_to_row,parent,false);
         return new FromToRvViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FromToRvViewHolder holder, int position) {
+
+        holder.cityName.setText("Dubai");
+        holder.cityCode.setText("DUB");
+        holder.countryName.setText("United Emirates");
 
     }
 
@@ -34,12 +39,14 @@ public class FromToRvAdapter extends RecyclerView.Adapter<FromToRvAdapter.FromTo
 
     public class FromToRvViewHolder extends RecyclerView.ViewHolder{
 
-        EditText searchCity;
+        TextView cityName , cityCode , countryName;
 
         public FromToRvViewHolder(@NonNull View itemView) {
 
             super(itemView);
-            searchCity=itemView.findViewById(R.id.et);
+            cityName=itemView.findViewById(R.id.city_name);
+            cityCode=itemView.findViewById(R.id.city_code);
+            countryName=itemView.findViewById(R.id.country_name);
         }
     }
 }
