74cfccc59287f871950a47119049ea87321d0032
mohammed <Middle-east1>
2019-07-16 17:40:47 +0200

    16/7 changes

---
+++ app/src/main/java/net/middledleeast/tamm/activities/RecommendedOneWay.java
@@ -0,0 +1,16 @@
+package net.middledleeast.tamm.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.middledleeast.tamm.R;
+
+public class RecommendedOneWay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recommended__one_way);
+    }
+}
