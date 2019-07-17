ce1eea34659bfa04568784d48e8e552e00d4b5a5
Nahas <info@middledleast.net>
2019-07-17 10:54:20 +0200

    17/7 just to pull
    Signed-off-by: Nahas <info@middledleast.net>

--- app/src/main/java/net/middledleeast/tamm/adapters/FromToRvAdapter.java
+++ app/src/main/java/net/middledleeast/tamm/adapters/FromToRvAdapter.java
@@ -19,7 +19,7 @@ public class FromToRvAdapter extends RecyclerView.Adapter<FromToRvAdapter.FromTo
     @NonNull
     @Override
     public FromToRvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.from_to_row,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.from_to_row, parent, false);
         return new FromToRvViewHolder(view);
     }
 
@@ -37,16 +37,16 @@ public class FromToRvAdapter extends RecyclerView.Adapter<FromToRvAdapter.FromTo
         return 0;
     }
 
-    public class FromToRvViewHolder extends RecyclerView.ViewHolder{
+    public class FromToRvViewHolder extends RecyclerView.ViewHolder {
 
-        TextView cityName , cityCode , countryName;
+        TextView cityName, cityCode, countryName;
 
         public FromToRvViewHolder(@NonNull View itemView) {
 
             super(itemView);
-            cityName=itemView.findViewById(R.id.city_name);
-            cityCode=itemView.findViewById(R.id.city_code);
-            countryName=itemView.findViewById(R.id.country_name);
+            cityName = itemView.findViewById(R.id.city_name);
+            cityCode = itemView.findViewById(R.id.city_code);
+            countryName = itemView.findViewById(R.id.country_name);
         }
     }
 }
