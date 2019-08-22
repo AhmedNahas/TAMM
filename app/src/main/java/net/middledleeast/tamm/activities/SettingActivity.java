5734e5e749597950b22c31d02275ae3e573e79cf
mohammed <Middle-east1>
2019-08-22 11:27:26 +0200

    SettingNew

--- app/src/main/java/net/middledleeast/tamm/activities/SettingActivity.java
+++ app/src/main/java/net/middledleeast/tamm/activities/SettingActivity.java
@@ -1,19 +1,156 @@
 package net.middledleeast.tamm.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.middledleeast.tamm.R;
 
 public class SettingActivity extends AppCompatActivity {
 
+    private Button english,arabic,on,off;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        english=findViewById(R.id.btn_english);
+        arabic=findViewById(R.id.btn_arabic);
+
+        on=findViewById(R.id.btn_ON);
+        off=findViewById(R.id.btn_off);
+
+
+
+        english.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    english.setBackground(getDrawable(R.color.app_color));
+                    english.setTextColor(0xFF000000);
+                    english.setTypeface(null, Typeface.BOLD_ITALIC);
+
+
+
+                    arabic.setBackground(getDrawable(R.color.float_transparent));
+                    arabic.setTextColor(0xFFBE973B);
+
+
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+
+
+        arabic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    arabic.setBackground(getDrawable(R.color.app_color));
+                    arabic.setTextColor(0xFF000000);
+                    arabic.setTypeface(null, Typeface.BOLD_ITALIC);
+
+
+
+                    english.setBackground(getDrawable(R.color.float_transparent));
+                    english.setTextColor(0xFFBE973B);
+                    english.setTypeface(null, Typeface.SANS_SERIF.getStyle());
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+
+
+        on.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    on.setBackground(getDrawable(R.color.app_color));
+                    on.setTextColor(0xFF000000);
+                    on.setTypeface(null, Typeface.BOLD_ITALIC);
+
+
+
+                    off.setBackground(getDrawable(R.color.float_transparent));
+                    off.setTextColor(0xFFBE973B);
+                    off.setTypeface(null, Typeface.NORMAL);
+
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+
+
+        off.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    off.setBackground(getDrawable(R.color.app_color));
+                    off.setTextColor(0xFF000000);
+                    off.setTypeface(null, Typeface.BOLD_ITALIC);
+
+
+
+                    on.setBackground(getDrawable(R.color.float_transparent));
+                    on.setTextColor(0xFFBE973B);
+                    on.setTypeface(null, Typeface.NORMAL);
+
+
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
+
+
+
+
+
+
+
 }
